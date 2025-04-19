17896dc1d62cdff7b99e290a56c2df60048af100
sumyka <129905991+kotoHa-cloud@users.noreply.github.com>
2025-04-19 17:54:19 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('about');
+
+    button.addEventListener('click', function () {
+        event.preventDefault();
+        alert('まだそのページは作ってません！！！')
+    })
+});
